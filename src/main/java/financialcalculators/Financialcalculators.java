14da91d97e0a4e7ca038f9bf9ebb9b2c24c19930
Yordiana0912 <Yordiana0912@gmail.com>
2024-04-15 13:01:27 -0400

    completed 2nd assignment CD cal

--- src/main/java/financialcalculators/Financialcalculators.java
+++ src/main/java/financialcalculators/Financialcalculators.java
@@ -7,29 +7,57 @@ import java.util.Scanner;
 
 public class Financialcalculators {
     final int MONTH_IN_YEAR = 12 ;
-    public static void main(String[] args) {Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter principal amount ");
-        double principal = scanner.nextDouble();
+    public static void main(String[] args) {
 
-        System.out.println("Enter annual interest rate ");
-        float annualInterestRate = scanner.nextFloat();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the terms in years: ");
-        int termsInYears = scanner.nextInt();
+        System.out.println(" Which Calcuator would you like to use? ");
+        System.out.println(" 1. Mortgage Calculator " );
+        System.out.println(" 2. CD Calculator"  );
+        int calculator = scanner.nextInt();
 
-        float monthlyInterestRate = annualInterestRate / 12 ;
-        int numberOfPayments = termsInYears * 12 ;
+        if( calculator == 1) {
 
-        double monthlyPayment = principal * (
-                (monthlyInterestRate * (Math.pow(1 + monthlyInterestRate , numberOfPayments)) /
-                        ((Math.pow( 1 + monthlyInterestRate, numberOfPayments)-1)
-                )));
-         System.out.println("Monthly payment: " + monthlyPayment);
+
+            System.out.println("Enter principal amount ");
+            double principal = scanner.nextDouble();
+
+            System.out.println("Enter annual interest rate ");
+            float annualInterestRate = scanner.nextFloat();
+
+
+            System.out.println("Enter the terms in years: ");
+            int termsInYears = scanner.nextInt();
+
+            double convertinterestrate = annualInterestRate / 100;
+            double monthlyInterestRate = convertinterestrate / 12;
+            int numberOfPayments = termsInYears * 12;
+
+            double monthlyPayment = principal * (
+                    (monthlyInterestRate * (Math.pow(1 + monthlyInterestRate, numberOfPayments)) /
+                            ((Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1)
+                            )));
+            System.out.println("Monthly payment: " + monthlyPayment);
+        } else if (calculator == 2) {
+
+            System.out.println(" How much would you like to deposit into your CD account? ");
+            double amountdeposit = scanner.nextInt();
+
+            System.out.println(" Please enter CD rate ? ");
+            float cdvalue = scanner.nextFloat();
+
+            System.out.println(" How many years is your CD term ?");
+            int cdterms = scanner.nextInt();
+
+            double convertCdrate = cdvalue / 100;
+
+            double futurevalue = amountdeposit *
+                    ((Math.pow( 1 + convertCdrate , cdterms )));
+            System.out.println("Future Value: "+ futurevalue);
+        }
+
+        }
+
     }
 
 
-
-
-
-
-}
